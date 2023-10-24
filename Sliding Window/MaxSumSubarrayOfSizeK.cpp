fa8734bf31df037c1a60c3872794dfe6eae3f03a
Mayank Mani Pant <102402423+mayankmanipant@users.noreply.github.com>
2023-10-24 11:27:58 +0530

    Create MaxSumSubarrayOfSizeK.cpp

---
+++ Sliding Window/MaxSumSubarrayOfSizeK.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int k, vector<int> &arr , int N){
+        // code here 
+        long m=INT_MIN;
+        long sum=0;
+        int i=0,j=0;
+        while(j < N){
+            sum+=arr[j];
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
+                m=max(m,sum);
+                sum-=arr[i];
+                i++;
+                j++;
+            }
+        }
+        return m;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
